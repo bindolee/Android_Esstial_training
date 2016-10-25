3ecdfc5050047a4d280690e97491d89510047743
bindolee <bindolee@gmail.com>
2016-10-24 22:20:27 -0700

    Adding code to control run time layout code and its containers from java code

--- app/src/main/java/essential/sbin/com/sbin_app/MainActivity.java
+++ app/src/main/java/essential/sbin/com/sbin_app/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.content_layout);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT );
+
+        for (int i=0; i<3; i++){
+            Button button = new Button(this);
+            button.setText("Click Me");
+            button.setLayoutParams(params);
+            layout.addView(button);
+        }
     }
 
     @Override
